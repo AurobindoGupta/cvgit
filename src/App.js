7d790039816528264dc2ff71015c29d46e058ee0
AurobindoGupta <78882138+AurobindoGupta@users.noreply.github.com>
2021-04-07 01:07:40 +0530

    edu

--- src/App.js
+++ src/App.js
@@ -5,31 +5,53 @@ export default function App() {
   var [userStuff, setUserStuff] = useState("");
 
   function userStuffHandler(event) {
-if(event.target.name==="name"){
-    userStuff = event.target.value;
-    setUserStuff(userStuff);
-    const uName= event.target.value;
-    console.log(uName);
+    if (event.target.name === "name") {
+      userStuff = event.target.value;
+      setUserStuff(userStuff);
+      const uName = event.target.value;
+      console.log(uName);
+    } else if (event.target.name === "email") {
+      const uEmail = event.target.value;
+      console.log(uEmail);
+    } else if (event.target.name === "pnumber") {
+      const uPnumber = event.target.value;
+      console.log(uPnumber);
+    }
   }
-  else if(event.target.name==="email"){
-    const uEmail= event.target.value;
-    console.log(uEmail);
-  }
-  else if(event.target.name==="pnumber"){
-    const uPnumber= event.target.value;
-    console.log(uPnumber);
-
-  }}
-  function submitButtonHandler(event){
-  // console.log(uName,uEmail,uPnumber);
-
+  function submitButtonHandler(event) {
+    // console.log(uName,uEmail,uPnumber);
   }
   return (
     <div className="App">
       <h1>Hello {userStuff}</h1>
-      <input placeholder="NAME" name="name" type="text"onChange={userStuffHandler}></input><br/>
-      <input placeholder="EMAIL" name="email"  type="email"onChange={userStuffHandler}/><br/>
-      <input placeholder="PHONE NUMBER" name="pnumber"type="number"onChange={userStuffHandler}></input><br/>
+      <input
+        placeholder="NAME"
+        name="name"
+        type="text"
+        onChange={userStuffHandler}
+      ></input>
+      <br />
+      <input
+        placeholder="EMAIL"
+        name="email"
+        type="email"
+        onChange={userStuffHandler}
+      />
+      <br />
+      <input
+        placeholder="PHONE NUMBER"
+        name="pnumber"
+        type="number"
+        onChange={userStuffHandler}
+      ></input>
+      <br />
+      <input
+        placeholder="EDUCATION"
+        name="education"
+        type="number"
+        onChange={userStuffHandler}
+      ></input>
+      <br />
       <button onClick={submitButtonHandler}>SUBMIT</button>
     </div>
   );
